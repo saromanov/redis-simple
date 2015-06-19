695a0b3cf806b948a85341526af02a734b5b50cb
Sergey Romanov <xxsmotur@gmail.com>
2015-06-19 20:35:12 +0500

    formatting

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
 var redis = require('redis');
 var client = redis.createClient();
 
-module.exports = function(){
+module.exports = function() {
     return {
-        set: function(key, value, hash, cb){
-            if(typeof hash === 'function'){
+        set: function(key, value, hash, cb) {
+            if (typeof hash === 'function') {
                 cb = hash;
                 hash = undefined;
             }
-            if(hash !== undefined){
-                client.hset(hash, key, value, function(err, reply){
-                    cb(err, reply);  
+            if (hash !== undefined) {
+                client.hset(hash, key, value, function(err, reply) {
+                    cb(err, reply);
                 });
-            }
-            else {
-                client.set(key, value, function(err, reply){
+            } else {
+                client.set(key, value, function(err, reply) {
                     cb(err, reply);
                 });
             }
         },
 
         get: function(key, hash, cb) {
-            if(typeof hash === 'function'){
+            if (typeof hash === 'function') {
                 cb = hash;
                 hash = undefined;
             }
-            if(hash !== undefined){
-                client.hget(hash, key, function(err, reply){
+            if (hash !== undefined) {
+                client.hget(hash, key, function(err, reply) {
                     cb(err, reply);
                 });
             } else {
-                client.get(key, function(err, reply){
+                client.get(key, function(err, reply) {
                     cb(err, reply);
                 });
             }
         },
 
-        del: function(key, field, cb){
-            if (typeof field === 'function'){
+        del: function(key, field, cb) {
+            if (typeof field === 'function') {
                 cb = field;
                 field = undefined;
             }
 
-            if(field !== undefined){
-                client.hdel(field, key, function(err, reply){
+            if (field !== undefined) {
+                client.hdel(field, key, function(err, reply) {
                     cb(err, reply);
                 });
             } else {
-                client.del(key, function(err, reply){
+                client.del(key, function(err, reply) {
                     cb(err, reply);
                 });
             }
         },
 
-        quit: function(){
+        quit: function() {
             client.quit();
         }
     };
